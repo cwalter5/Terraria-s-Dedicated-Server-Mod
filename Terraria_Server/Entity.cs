9fd68f1679584d0fb8f1caf59f37fd97940c87b9
Chris Walter <chrisdubz@gmail.com>
2011-07-05 09:19:13 -0400

    Forgot to commit base class entity

---
+++ Terraria_Server/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Terraria_Server.Misc;
+
+namespace Terraria_Server
+{
+    public class Entity
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public bool Active;
+    }
+}
